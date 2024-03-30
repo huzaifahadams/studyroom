4ac9846268c79b6171918c7526f1bf4ae89606da
huzaifahadams <huzaifah.adams@gmail.com>
2024-03-31 01:48:44 +0300

    comment is done but no styling

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .models import Room,Topic
+from .models import Room,Topic,Messages
 from django. db.models import Q
 from .forms import RoomForm
 from django.http import HttpResponse
@@ -70,6 +70,17 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.messages_set.all().order_by('-created')
+    
+    if request.method == 'POST':
+        message = Messages.objects.create(
+            user= request.user,
+            room=room,
+            body=request.POST.get('body'),
+            
+        )
+        return redirect('room',pk=room.id)
+    
+        
     context = {'room': room,'room_messages':room_messages}
     return  render(request,'core/rooms.html',context)
 
