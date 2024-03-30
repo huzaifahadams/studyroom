dac6da66b6242d935fcee57cc21d4aaf636dd530
huzaifahadams <huzaifah.adams@gmail.com>
2024-03-31 01:17:59 +0300

    working on messages/comments

--- core/views.py
+++ core/views.py
@@ -68,9 +68,9 @@ def home(request):
 
 
 def room(request, pk):
-    room = Room
-    context = {'room': room}
-
+    room = Room.objects.get(id=pk)
+    room_messages = room.messages_set.all().order_by('-created')
+    context = {'room': room,'room_messages':room_messages}
     return  render(request,'core/rooms.html',context)
 
 @login_required(login_url='login')
